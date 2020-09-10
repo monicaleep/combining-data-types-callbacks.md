3a5777ebfd3aca850d2858d0e8a6fa07a5ef66bb
monicaleep <monica.parrillo@gmail.com>
2020-09-10 18:52:00 -0500

    PRODUCTS

--- array_w_callbacks.js
+++ array_w_callbacks.js
@@ -170,10 +170,128 @@ function isPanagram(array) {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz';
   // turn alphabet into an array and use the every function to check if every letter in alphabet is in panagram;
   return alphabet.split("").every(letter => {
-    // return true if the letter (from alphabet) is in the provided array
+    // return true if the provided array includes the letter
     return (array.join(" ").toLowerCase().includes(letter))
   })
 }
 //;
 isPanagram(panagram) // true
 isPanagram(['a']) // false
+isPanagram(['abcdefghijklmnopqrstuvxyz']) // false (I removed the 'w')
+
+
+// =========================================
+// P R O D U C T S
+// =========================================
+
+const products = [
+      {
+        "name": "allen wrench",
+        "price": 2.99,
+        "description": "handy tool"
+      },
+      {
+        "name": "cornucopia",
+        "price": 5.99,
+        "description": "festive holiday decoration"
+      },
+      {
+        "name": "banana",
+        "price": 0.99,
+        "description": "full of potassium"
+      },
+      {
+        "name": "guillotine (cigar)",
+        "price": 10.59,
+        "description": "behead your stub"
+      },
+      {
+        "name": "jack-o-lantern",
+        "price": 3.99,
+        "description": "spooky seasonal fun"
+      },
+      {
+        "name": "doggie treat (box)",
+        "price": 5.99,
+        "description": "fido loves 'em"
+      },
+      {
+        "name": "egg separator",
+        "price": 3.99,
+        "description": "it separates yolks from whites"
+      },
+      {
+        "name": "LED lightbulb",
+        "price": 6.55,
+        "description": "It's super-efficient!"
+      },
+      {
+        "name": "owl pellets",
+        "price": 3.99,
+        "description": "Don't ask what they used to be."
+      },
+      {
+        "name": "flag",
+        "price": 5.99,
+        "description": "catches the breeze"
+      },
+      {
+        "name": "hair brush",
+        "price": 6.99,
+        "description": "fine boar bristles"
+      },
+      {
+        "name": "iridium (one gram)",
+        "price": 19.36,
+        "description": "corrosion-resistant metal"
+      },
+      {
+        "name": "quark",
+        "price": 0.01,
+        "description": "Very small"
+      },
+      {
+        "name": "turtleneck",
+        "price": 19.99,
+        "description": "available in black and slightly-darker black"
+      },
+      {
+        "name": "kaleidoscope",
+        "price": 8.25,
+        "description": "tube with moving plastic pieces inside"
+      },
+      {
+        "name": "mitt (leather)",
+        "price": 15,
+        "description": "regulation sized"
+      },
+      {
+        "name": "nothing",
+        "price": 10,
+        "description": "Hey, if you pay us, we won't ask any questions."
+      },
+      {
+        "name": "violin",
+        "price": 2000,
+        "description": "Talk about a JS fiddle..."
+      },
+      {
+        "name": "yoyo",
+        "price": 1,
+        "description": "We had to pull some strings to get this one in."
+      },
+      {
+        "name": "pincushion",
+        "price": 1.99,
+        "description": "You'll get 'stuck' on it"
+      },
+    ]
+
+
+//filter for products with a price that is less than 10, using the array below:
+products.filter(product=>product.price<10);
+
+//sort alphabetically by product name
+products.sort((first, second)=>{
+  return first.name.toLowerCase() - second.name.toLowerCase()
+})
